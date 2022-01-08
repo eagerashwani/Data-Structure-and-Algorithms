45249ca1f1a5ef3bf0678a70f215d4e61af50f1e
Ashwani Kumar <akemma1541990@gmail.com>
2022-01-08 21:19:23 +0530

    added

--- Basics/gradingsystem.java
+++ Basics/gradingsystem.java
@@ -1,3 +1,4 @@
+package Basics;
 public class gradingsystem {
     public static void main(String[] args) {
         
---
+++ functions/digitFreq/DigitFreq.java
@@ -0,0 +1,25 @@
+package functions.digitFreq;
+
+import java.util.Scanner;
+
+public class DigitFreq {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+        int f = getDigitFrequency(n, d);
+        System.out.println(f);
+    }
+
+    public static int getDigitFrequency(int n, int d) {
+        int temp = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            if (rem == d) {
+                temp++;
+            }
+        }
+        return temp;
+    }
+}
